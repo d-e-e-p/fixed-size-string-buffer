90316691ffa3c59b2c90e6c7d35052f3e7e26347
d-e-e-p <deep@tensorfield.ag>
2023-03-17 10:22:02 -0700

    add implementation of char limited queue using std::queue as base class

---
+++ include/fixed_size_queue.h
@@ -0,0 +1,51 @@
+
+/*
+ from https://stackoverflow.com/questions/56334492/c-create-fixed-size-queue
+ Usage:
+    FixedQueue<string, 100> cmdHistory;
+*/
+
+#include <queue>
+#include <utility>
+#include <iostream>
+
+class FixedQueue : public std::queue<std::string> {
+
+private:
+  size_t free_space_ = 0;
+  std::queue<size_t> strsize_ = {}; // pointer to start/length of str in chars
+  using std::queue<std::string>::swap; // disallow
+
+public:
+  explicit FixedQueue(size_t SIZE)
+      : free_space_(SIZE)
+  {
+  }
+
+  void push(const std::string& str);  
+  void pop();
+};
+
+
+inline void FixedQueue::push(const std::string& str) 
+{
+  // clear space for str
+  size_t strlen = str.length();
+  while (free_space_ < strlen) {
+    free_space_ += strsize_.front();
+    strsize_.pop();
+    std::queue<std::string>::pop();
+  }
+
+  strsize_.push(strlen);
+  std::queue<std::string>::push(str);
+  free_space_ -= strlen;
+}
+
+inline void FixedQueue::pop() 
+{
+  free_space_ += strsize_.front();
+  strsize_.pop();
+  std::queue<std::string>::pop(); 
+}
+
