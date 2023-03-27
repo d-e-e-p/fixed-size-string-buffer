20b46cd1833bf7f1bf11cb8d398df072b2810a41
d-e-e-p <deep@tensorfield.ag>
2023-03-27 04:54:08 -0700

    force c++17

--- include/fixed_size_string_buffer.h
+++ include/fixed_size_string_buffer.h
@@ -18,8 +18,8 @@
 #pragma once
 #pragma clang diagnostic ignored "-Wnarrowing"
 
-#include <string_view>
 #include <string>
+#include <string_view>
 #include <array>
 #include <vector>
 #include <map>
